affabbb83e84ce4585cf37f8ef5f73c85e9870c0
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-06-03 21:26:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  
+  
 });
+
+
+let input
+
+function submit(){
+  
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+}, false);
+
+document.addEventListener("keydown",event => {
+  
+  if (event.keyCode === 13) {
+    let input = document.getElementById("new-task-description").value
+    document.getElementById("new-task-description").value = ""
+  }
+  
+});
+
+
+
+
